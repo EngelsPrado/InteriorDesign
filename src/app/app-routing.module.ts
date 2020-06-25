10f243e773046f61dc2bb088b38313c254561b18
Engels Prado <engelslove09@gmail.com>
2020-06-25 17:44:41 -0500

    Agregado componentes:Blog,about y contact

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
+import {ContactComponent} from './components/contacts/contact/contact.component'
+import {AppComponent} from './app.component'
+import {HomeComponent} from './components/home/home.component'
+import {AboutComponent} from './components/about/about.component'
+import {BlogComponent} from './components/blog/blog.component'
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  { path: 'Contact', component: ContactComponent },
+  { path: 'About', component: AboutComponent },
+  { path: 'Blog', component: BlogComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactComponent } from './components/contacts/contact/contact.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { BlogComponent } from './components/blog/blog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactComponent,
+    HomeComponent,
+    AboutComponent,
+    BlogComponent
   ],
   imports: [
     BrowserModule,
